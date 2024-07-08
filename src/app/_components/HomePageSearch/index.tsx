d9e23612ffdb58457d8fbd73030e41c0e81277b2
kalinchkma <chakma.kalin10211@gmail.com>
2024-07-09 02:51:11 +0600

    working with home page

--- src/app/_components/HomePageSearch/index.tsx
+++ src/app/_components/HomePageSearch/index.tsx
@@ -4,7 +4,9 @@ import { Gutter } from '../Gutter'
 export const HomePageSearch = () => {
   return (
     <Gutter>
-        <input placeholder='Search products...' />
+        <div className='flex items-center justify-center'>
+            <input placeholder='Search products...' className='w-[90%]' />
+        </div>
     </Gutter>
   )
 }
